35d1a2c18ccbf316c7406b862a16da0dc69c20da
marco <marco.antonio.rg340@gmail.com>
2024-10-01 19:09:46 -0300

    EmailImp

--- src/main/java/infnet/edu/apibloco/Infrastructure/Services/EmailSenderService.java
+++ src/main/java/infnet/edu/apibloco/Infrastructure/Services/EmailSenderService.java
@@ -6,22 +6,38 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import infnet.edu.apibloco.Domain.Contracts.Email.SendEmailRequest;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class EmailSenderService {
 
-    private WebClient CreateWebClient() {
+    private final String Base = "http://localhost:8080/api/Email/";
+    private final String PostEndpoint = Base + "send";
+    private final String GetByEmailEndpoint = Base + "getEmails";
+    private final String DeleteEmails = Base + "delete";
+
+    private WebClient CreateWebClient(String endpoint) {
+
+        
+
         WebClient client = WebClient.builder()
-                .baseUrl("http://localhost:8080/api/Email/send")
+                .baseUrl(Base + endpoint)
                 .build();
 
         return client;
     }
     
+    public Flux<String>GetEmailsByUser(String emailAddress)
+    {
+        WebClient client = CreateWebClient(GetByEmailEndpoint);
+
+        return client.get().retrieve().bodyToFlux(String.class);
+    }
+
     public Mono<String> SendEmail(SendEmailRequest request)
     {
-        WebClient client = CreateWebClient();
+        WebClient client = CreateWebClient(PostEndpoint);
 
         return client.post()
         .contentType(MediaType.APPLICATION_JSON)
